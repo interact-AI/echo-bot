a4f19e38a7b68e1f2bd1dbe3dfa912d705010e21
Manuel Barrabino <manubarrabino@gmail.com>
2024-06-27 15:43:49 -0300

    no se que 80

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ import os
 class DefaultConfig:
     """ Bot Configuration """
 
-    PORT = 8000
+    PORT = 80
     APP_ID = os.environ.get("MicrosoftAppId", "31e30c1d-c3a8-465c-80c1-988b2baabbc0")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "zs~8Q~aEhyJPuNFtyHzEOQsBM-wdLNfx0ZvEYalq")
     APP_TYPE = os.environ.get("MicrosoftAppType", "MultiTenant")

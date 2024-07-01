44312a90d6d203a0262f879a05afceb60c760105
Manuel Barrabino <manubarrabino@gmail.com>
2024-07-01 16:58:25 -0300

    con flask intermedierio

--- app.py
+++ app.py
@@ -1,99 +1,29 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
+from flask import Flask, request, jsonify
+from langgraph_bot import execute_agent
+import time
 
-import sys
-import traceback
-from datetime import datetime
-from http import HTTPStatus
+app = Flask(__name__)
 
-from aiohttp import web
-from aiohttp.web import Request, Response, json_response, Application, run_app
-from botbuilder.core import (
-    TurnContext,
-)
-from botbuilder.core.integration import aiohttp_error_middleware
-from botbuilder.integration.aiohttp import CloudAdapter, ConfigurationBotFrameworkAuthentication
-from botbuilder.schema import Activity, ActivityTypes
+@app.route('/status', methods=['GET'])
+def status():
+    return 'The server is up and running!'
 
-from bots import EchoBot
-from config import DefaultConfig
-
-CONFIG = DefaultConfig()
-
-# Create adapter.
-# See https://aka.ms/about-bot-adapter to learn more about how bots work.
-ADAPTER = CloudAdapter(ConfigurationBotFrameworkAuthentication(CONFIG))
-
-
-# Catch-all for errors.
-async def on_error(context: TurnContext, error: Exception):
-    # This check writes out errors to console log .vs. app insights.
-    # NOTE: In production environment, you should consider logging this to Azure
-    #       application insights.
-    print(f"\n [on_turn_error] unhandled error: {error}", file=sys.stderr)
-    traceback.print_exc()
-
-    # Send a message to the user
-    await context.send_activity("The bot encountered an error or bug.")
-    await context.send_activity(
-        "To continue to run this bot, please fix the bot source code."
+@app.route('/message', methods=['POST'])
+def message():
+    message = request.json.get('message')
+    conversation_id = request.headers.get('conversationId')
+    initial_time = time.time()
+    result = execute_agent(
+            message, conversation_id
     )
-    # Send a trace activity if we're talking to the Bot Framework Emulator
-    if context.activity.channel_id == "emulator":
-        # Create a trace activity that contains the error object
-        trace_activity = Activity(
-            label="TurnError",
-            name="on_turn_error Trace",
-            timestamp=datetime.utcnow(),
-            type=ActivityTypes.trace,
-            value=f"{error}",
-            value_type="https://www.botframework.com/schemas/error",
-        )
-        # Send a trace activity, which will be displayed in Bot Framework Emulator
-        await context.send_activity(trace_activity)
+    print(f"Tiempo de respuesta de Agente entero: {time.time() - initial_time}")
+
+    response = {
+        'response': result["final_response"]
+    }
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=80, ssl_context=('cert.pem', 'key.pem'))
 
 
-ADAPTER.on_turn_error = on_error
-
-# Create the Bot
-BOT = EchoBot()
-
-async def status(req: Request) -> Response:
-    print("Received status request")
-    return Response(status=HTTPStatus.OK, text="Healthy")
-
-
-# Listen for incoming requests on /api/messages
-async def messages(req: Request) -> Response:
-    # Main bot message handler.
-    if "application/json" in req.headers["Content-Type"]:
-        body = await req.json()
-    else:
-        return Response(status=HTTPStatus.UNSUPPORTED_MEDIA_TYPE)
-
-    activity = Activity().deserialize(body)
-    auth_header = req.headers["Authorization"] if "Authorization" in req.headers else ""
-
-    response = await ADAPTER.process_activity(auth_header, activity, BOT.on_turn)
-    if response:
-        return json_response(data=response.body, status=response.status)
-    return Response(status=HTTPStatus.OK)
-
-async def ping(req: Request) -> Response:
-    print("Received ping request")
-    return Response(status=HTTPStatus.OK, text="pong")
-
-
-def init_func(argv):
-    APP = Application(middlewares=[aiohttp_error_middleware])
-    APP.router.add_post("/api/messages", messages)
-    APP.router.add_get("/status", status)
-    APP.router.add_get("/ping", ping)
-    return APP
-
-if __name__ == "__main__":
-    APP = init_func(None)
-    try:
-        run_app(APP, host="0.0.0.0", port=CONFIG.PORT)
-    except Exception as error:
-        raise error
